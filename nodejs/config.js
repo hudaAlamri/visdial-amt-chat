36824dfa95834715838afd09feceac1c07b6c5e5
hudaAlamri <halamri3@gatech.edu>
2017-11-10 12:31:14 -0500

    adding kinetic database

---
+++ nodejs/config.js
@@ -0,0 +1,17 @@
+var config = {}
+
+config.db = {
+    "user": "halamri",
+    "name": "testdb",
+    "pass": "awaaqa90"
+}
+
+config.redis = {
+    "port": 6379,
+    "pass": "",
+    "list": "visdial_queue"
+}
+
+config.root = '/home/hudalamri/visdial-amt-chat/nodej'
+
+module.exports = config
